e14a5a0cd4963e0d63c1da81de9992057aba66a4
junjiezh <zjj476778843@gmail.com>
2025-08-01 17:34:06 +0800

    wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,36 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.keys = {
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
+
+config.font_size = 14
+
+config.enable_tab_bar = true
+config.window_decorations = "TITLE | RESIZE"
+config.color_scheme = "Catppuccin Mocha"
+
+config.window_padding = {
+	bottom = 0,
+}
+config.native_macos_fullscreen_mode = true
+config.prefer_to_spawn_tabs = true
+
+
+-- set up max window on startup
+-- local mux = wezterm.mux
+-- wezterm.on("gui-startup", function(cmd)
+-- 	local tab, pane, window = mux.spawn_window(cmd or {})
+-- 	window:gui_window():maximize()
+-- end)
+
+-- and finally, return the configuration to wezterm
+return config
